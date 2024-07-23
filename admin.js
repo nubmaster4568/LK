cef1c301d9b85dea0bc9f5215273866d26f276f2
nubmaster4568 <144072936+nubmaster4568@users.noreply.github.com>
2024-07-23 12:54:04 -0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -2,18 +2,20 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '6911216755:AAGlDbzA5WLa0bZlMk2iv3dx7T2vko55sXc';
 const bot = new TelegramBot(token, { polling: true });
 
-bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id; // Corrected variable name
-  bot.sendMessage(chatId, 'SHOP', {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          {
-            text: 'SHOP',
-            web_app: { url: 'https://lk-rt5d.onrender.com/admin/index.html' }
-          }
-        ]
-      ]
-    }
-  });
+bot.start((ctx) => {
+    const chatId = ctx.chat.id; // Get chat ID from the context
+    ctx.reply('SHOP', {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {
+                        text: 'SHOP',
+                        web_app: { url: 'https://site-45i5.onrender.com/admin/index.html' }
+                    }
+                ]
+            ]
+        }
+    });
 });
+
+bot.launch();
