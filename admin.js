0e35732054e7d52583019802a8ce43bc6abbe6f5
nubmaster4568 <144072936+nubmaster4568@users.noreply.github.com>
2024-07-23 12:23:09 -0700

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,19 @@
+const TelegramBot = require('node-telegram-bot-api');
+const token = '6911216755:AAGlDbzA5WLa0bZlMk2iv3dx7T2vko55sXc';
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id; // Corrected variable name
+  bot.sendMessage(chatId, 'SHOP', {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: 'SHOP',
+            web_app: { url: 'https://site-45i5.onrender.com/admin/index.html' }
+          }
+        ]
+      ]
+    }
+  });
+});
